f22f5050b4ef1214dc5d73956a7dbbfc1850f6f4
Bhavika Tibrewal <bhtibrewal@gmail.com>
2022-12-03 12:45:19 +0530

    test: passed all test cases

---
+++ src/components/starbucks-input/index.js
@@ -0,0 +1,8 @@
+const StarbucksInput = ({ name, value, onChange, error }) => {
+    return (< >
+        <label htmlFor={name}>{name}</label>
+        <input id={name} value={value} onChange={(e) => onChange(e.target.value)} />
+        {error && <div>{error}</div>}
+    </>)
+}
+export default StarbucksInput
